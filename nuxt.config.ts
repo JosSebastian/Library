284bdef12989b2ad795c841bdc7e8ff19c66134f
JosSebastian <sebastian.jos2222@gmail.com>
2024-06-03 14:05:44 -0500

    nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  srcDir: "source",
+  devtools: {
+    enabled: true,
+    timeline: {
+      enabled: true,
+    },
+  },
+});
