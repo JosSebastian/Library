49ba1dbd1e73c7dd59a70341eeee1e172df90028
JosSebastian <sebastian.jos2222@gmail.com>
2024-06-04 15:21:16 -0500

    pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+
   srcDir: "source",
+  imports: { dirs: ["stores"] },
+
+  modules: ["@pinia/nuxt"],
+
   devtools: {
     enabled: true,
     timeline: {
