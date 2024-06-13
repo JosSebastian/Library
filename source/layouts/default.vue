08dce9fb125bd15467aa15479d0cf564ebc6e948
JosSebastian <sebastian.jos2222@gmail.com>
2024-06-13 17:03:31 -0500

    initial home page

--- source/layouts/default.vue
+++ source/layouts/default.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
+  <div class="w-full max-w-screen h-full min-h-screen flex flex-col">
     <slot />
   </div>
 </template>
--- source/pages/index.vue
+++ source/pages/index.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div />
+  <div class="w-full h-full flex-grow flex flex-col">
+    <!-- Header -->
+    <header></header>
+
+    <!-- Body -->
+    <main class="flex-grow"></main>
+
+    <!-- Footer -->
+    <footer></footer>
+  </div>
 </template>
