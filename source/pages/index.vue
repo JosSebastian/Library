2bcb8acb5ec79f0eb7f1bb0c86d2f08c9c1a166a
JosSebastian <sebastian.jos2222@gmail.com>
2024-06-13 23:09:01 -0500

    header and footer

--- source/pages/index.vue
+++ source/pages/index.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="w-full h-full flex-grow flex flex-col">
+  <div class="w-full h-full m-0 p-3 flex-grow flex flex-col gap-3">
     <!-- Header -->
-    <header></header>
+    <header class="flex justify-between items-center">
+      <ULink class="text-3xl font-semibold">Library</ULink>
+      <UButton icon="i-heroicons-bars-3" variant="ghost" size="lg" />
+    </header>
+
+    <UDivider />
 
     <!-- Body -->
-    <main class="flex-grow"></main>
+    <main class="flex-grow">
+      <section />
+    </main>
+
+    <UDivider />
 
     <!-- Footer -->
-    <footer></footer>
+    <footer class="flex justify-center items-center">
+      <ULink>@Library</ULink>
+    </footer>
   </div>
 </template>
