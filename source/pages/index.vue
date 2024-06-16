5bb335b7291e09f0a9038a08f6b0194920ac3cba
JosSebastian <sebastian.jos2222@gmail.com>
2024-06-16 16:52:36 -0500

    section 3

--- source/pages/index.vue
+++ source/pages/index.vue
@@ -110,6 +110,88 @@
           </div>
         </div>
       </section>
+
+      <UDivider />
+
+      <!-- Section #3 -->
+      <section class="max-w-screen min-h-screen flex">
+        <div
+          class="p-0 md:px-24 md:py-12 flex-grow flex flex-col justify-evenly items-center gap-12"
+        >
+          <!-- Top -->
+          <div>
+            <div class="flex flex-col gap-6 text-center">
+              <p class="text-lg text-violet-600 dark:text-violet-400">
+                AUGUE ENIM ERAT
+              </p>
+              <p class="text-6xl font-bold">Auctor magnis tempus</p>
+              <p class="text-lg">
+                Mus leo nunc nullam lacus ata nibh tempor fermentum ultrices
+              </p>
+            </div>
+          </div>
+
+          <!-- Bottom -->
+          <div class="w-full h-fit flex flex-col items-center gap-12">
+            <div
+              class="w-full h-fit flex flex-col md:flex-row justify-evenly items-center gap-12 text-center"
+            >
+              <!-- Left -->
+              <div class="w-full md:w-1/2 h-fit flex justify-center">
+                <div class="w-full md:w-3/4 md:min-w-60 md:max-w-96">
+                  <img
+                    src="https://placehold.co/450x600"
+                    width="100%"
+                    height="100%"
+                  />
+                </div>
+              </div>
+
+              <!-- Right -->
+              <div
+                class="w-full md:w-1/2 h-full flex flex-col justify-evenly gap-6"
+              >
+                <div class="w-full h-fit flex flex-col gap-6">
+                  <p class="text-3xl font-semibold">Montes semper quam</p>
+                  <p class="text-lg">
+                    Mattis urna turpis nullam velit in vel feugiat malesuada
+                    orci eu auctor magnis odio nisi scelerisque adipiscing
+                    auctor.
+                  </p>
+                </div>
+
+                <UDivider />
+
+                <div class="w-full h-fit flex flex-col gap-6">
+                  <p class="text-3xl font-semibold">Montes semper quam</p>
+                  <p class="text-lg">
+                    Mattis urna turpis nullam velit in vel feugiat malesuada
+                    orci eu auctor magnis odio nisi scelerisque adipiscing
+                    auctor.
+                  </p>
+                </div>
+
+                <UDivider />
+
+                <div class="w-full h-fit flex flex-col gap-6">
+                  <p class="text-3xl font-semibold">Montes semper quam</p>
+                  <p class="text-lg">
+                    Mattis urna turpis nullam velit in vel feugiat malesuada
+                    orci eu auctor magnis odio nisi scelerisque adipiscing
+                    auctor.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <UButton variant="outline" size="lg" class="w-fit h-fit">
+                <p class="text-lg">Magna etiam lorem</p>
+              </UButton>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
 
     <UDivider />
