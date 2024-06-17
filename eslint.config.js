28dfa0b8692381131a489108ce3ba6600629c368
JosSebastian <sebastian.jos2222@gmail.com>
2024-06-17 15:20:18 -0500

    fixes

--- eslint.config.js
+++ eslint.config.js
@@ -4,5 +4,6 @@ import withNuxt from "./.nuxt/eslint.config.mjs";
 export default withNuxt().override("nuxt/vue/rules", {
   rules: {
     "vue/multi-word-component-names": "off",
+    "vue/html-self-closing": "off",
   },
 });
